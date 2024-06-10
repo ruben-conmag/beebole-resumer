1b20c0cbcd8e17f417f5cfe1a6871c356cd6dba1
rubencondemag96 <rubencondemag96@gmail.com>
2024-06-10 20:45:22 +0000

    ♻️ Log env variables

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import "dotenv/config";
 import { fetchData } from "./src/fetchData.js";
+const {
+    BASE64_AUTH_TOKEN,
+    USER_ID,
+    DISCORD_WEBHOOK_URL,
+    DISCORD_WEBHOOK_NAME
+} = process.env;
+
+console.log(BASE64_AUTH_TOKEN)
+console.log(USER_ID)
+console.log(DISCORD_WEBHOOK_URL)
+console.log(DISCORD_WEBHOOK_NAME)
 
 fetchData().catch((err) => {
     console.error(err);
